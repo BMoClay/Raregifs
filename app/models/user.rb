d3e7d54c88f170b7459c6d72eeffef5608b2877d
BMoClay <benmorgancleveland@gmail.com>
2021-02-23 19:14:59 -0500

    commented out line 4 in User model (# has_many :artworks, through: :acquisitions)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :artworks
     has_many :acquisitions
-    has_many :artworks, through: :acquisitions
+    # has_many :artworks, through: :acquisitions
+    # has_many :acquired_artworks
+    # has_many :collected_artworks, :through => :acquired_artworks, :source => "artwork"
 end
--- app/serializers/artwork_serializer.rb
+++ app/serializers/artwork_serializer.rb
@@ -2,6 +2,6 @@ class ArtworkSerializer < ActiveModel::Serializer
   belongs_to :user 
   has_many :acquisitions 
   has_many :users, :through :acquisitions
-  
+
   attributes :id, :title, :user_id, :image
 end
