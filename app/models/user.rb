d2800ece641e6a06a324d6d845efbc34568c2f46
BMoClay <benmorgancleveland@gmail.com>
2021-02-24 20:55:04 -0500

    uncomment bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_secure_password
+    validates :name, uniqueness: { case_sensitive: false }
     has_many :artworks
     has_many :acquisitions
     # has_many :artworks, through: :acquisitions
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create(name: "Ben Morgan-Cleveland", password: "123", email: "email")
 Artwork.create(user_id: 1, title: "Mona Lisa", image:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3be2c838-df3d-4cfa-9401-6b41e6136a19/d4ps09e-5ae2ab7f-06d3-450b-b1a3-981641477d3c.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvM2JlMmM4MzgtZGYzZC00Y2ZhLTk0MDEtNmI0MWU2MTM2YTE5XC9kNHBzMDllLTVhZTJhYjdmLTA2ZDMtNDUwYi1iMWEzLTk4MTY0MTQ3N2QzYy5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.blToWVRutAn62T8sjBhjRYBwGdUIzoILVBT_yKqXG0E") 
 
 
-User.create(name: "Morgan", password: "123", email: "email")
+User.create(name: "Morgan", password: "456", email: "email")
 Artwork.create(user_id: 2, title: "Gloucester", image:"https://fritsahlefeldt.files.wordpress.com/2019/09/30ec7-watercolor-sketch-ertholmene-christiansoe-by-frits-ahlefeldt.jpg?w=1200")     
 
 Acquisition.create(user_id: 1, artwork_id: 2)
