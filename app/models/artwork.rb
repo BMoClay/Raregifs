f4bf5cb205c3227488fd89e408746ccf2c34c039
BMoClay <benmorgancleveland@gmail.com>
2021-03-07 15:05:33 -0500

    added dependent destroys backend frontend functional

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,50 +1,9 @@
 class Artwork < ApplicationRecord
     belongs_to :user
-    has_many :acquisitions
+    has_many :acquisitions, dependent: :destroy
 
     def user_name
         self.user.name
     end
-
-
-
-
-
-
-
-
-
-
-
-
-    # has_many :users, through: :acquisitions
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # has_many :acquiring_users, through: :acquisitions
-
-    # has_many :shared_artworks, foreign_key: :acquisition_id
-    # has_many :acquisitions, foreign_key: :acquisition_id
-    # has_many :acquiring_users, through: :shared_artworks, source: :sub_user
-    # has_many :acquiring_users, through: :acquisitions, source: :sub_user
-
-    # belongs_to :user
-    # has_many :acquisitions
-    # has_many :acquiring_users, through: :acquisitions, source: :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,37 +2,17 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, uniqueness: { case_sensitive: false }
     has_many :artworks
-    has_many :acquisitions
+    has_many :acquisitions, dependent: :destroy
     
-   
-    has_many :comments_received, class_name: 'Comment', foreign_key: :commentee_id
-    has_many :commenting_users, through: :comments_received, source: :commenter
+    has_many :comments_received, class_name: 'Comment', foreign_key: :commentee_id, dependent: :destroy
+    has_many :commenting_users, through: :comments_received, source: :commenter, dependent: :destroy
     has_many :comments_made, class_name: 'Comment', foreign_key: :commenter_id
     has_many :commented_on_users, through: :comments_made, source: :commentee
 
-
-
-
-
-
-
-        # has_many :commenters, foreign_key: :commenter_id, class_name: 'Comment'
-    # has_many :comments_received, through: :commenter
-    # has_many :commentees, foreign_key: :commentee_id, class_name: 'Comment'
-    # has_many :comments_made, through: :commentees
-
-
-    # has_many :acquired_artworks, through: :acquisitions
-    
-    # has_many :created_artworks, foreign_key: :holder_id, class_name: 'Artwork'
-    # has_many :created_artworks, foreign_key: :user_id, class_name: 'Artwork'
-    # has_many :acquisitions, foreign_key: :sub_user_id
-    # has_many :shared_artworks, foreign_key: :artwork_id
-    # has_many :acquired_artworks, through: :acquisitions, source: :user
-   
-   
-    # has_many :artworks
-    # has_many :acquisitions
-    # has_many :acquired_artworks, through: :acquisitions, source: :artwork
-
 end
+
+# before_destroy :destroy_comments_received
+
+# def destroy_comments_received
+#     comments_received.destroy unless user.acquisitions
+# end
