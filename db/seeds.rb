a73e1d99592e6848fe11f3bac3d1d4a362b537d6
BMoClay <benmorgancleveland@gmail.com>
2021-07-04 12:05:21 -0400

    change order seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,6 @@ User.create(name: "Morgan", password: "456")
 User.create(name: "Adrienne", password: "789")
 User.create(name: "Sam", password: "666")
 
-Artwork.create(user_id: 1, title: "Mysterious mischief", image: "https://i.imgur.com/zM9FiHs.gif")
 Artwork.create(user_id: 2, title: "Mysf", image: "https://i.imgur.com/LRAqVJz.gif")
 Artwork.create(user_id: 1, title: "Falling in reverse", image:"https://i.imgur.com/Twlqjkx.gif")
 Artwork.create(user_id: 2, title: "Gloucester", image:"https://fritsahlefeldt.files.wordpress.com/2019/09/30ec7-watercolor-sketch-ertholmene-christiansoe-by-frits-ahlefeldt.jpg?w=1200")     
@@ -21,6 +20,7 @@ Artwork.create(user_id: 3, title: "who's afraid of red", image: "https://media2.
 Artwork.create(user_id: 3, title: "wink", image: "https://media.giphy.com/media/l41YaEfCkjCyHl1Oo/giphy.gif")
 Artwork.create(user_id: 1, title: "eating my food", image: "https://media.giphy.com/media/3o8doM1LK2PVaxawpO/giphy.gif")
 Artwork.create(user_id: 1, title: "Mona Lisa", image:"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3be2c838-df3d-4cfa-9401-6b41e6136a19/d4ps09e-5ae2ab7f-06d3-450b-b1a3-981641477d3c.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvM2JlMmM4MzgtZGYzZC00Y2ZhLTk0MDEtNmI0MWU2MTM2YTE5XC9kNHBzMDllLTVhZTJhYjdmLTA2ZDMtNDUwYi1iMWEzLTk4MTY0MTQ3N2QzYy5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.blToWVRutAn62T8sjBhjRYBwGdUIzoILVBT_yKqXG0E") 
+Artwork.create(user_id: 1, title: "Mysterious mischief", image: "https://i.imgur.com/zM9FiHs.gif")
 Artwork.create(user_id: 1, title: "Hello!", image: "https://i.imgur.com/oMVgsA9.gif")
 
 # Artwork.create(user_id: 4, title: "got it", image: "https://media.giphy.com/media/3o6ZsSRkXifoEJfpMA/giphy.gif")
