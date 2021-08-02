dc8fdac0a81bda104f3d0baf1020bd7318b36835
BMoClay <benmorgancleveland@gmail.com>
2021-08-02 13:17:32 -0400

    change cors to origin raregifs bla

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://keen-stonebraker-703779.netlify.app'
+    origins 'https://raregifs.netlify.app'
 
     resource '*',
       headers: :any,
