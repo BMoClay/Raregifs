6758bd25d3ea633a3869bc500fb0d100e9b79ad3
BMoClay <benmorgancleveland@gmail.com>
2021-08-02 12:53:42 -0400

    change cors to netflify app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://keen-stonebraker-703779.netlify.app'
 
     resource '*',
       headers: :any,
