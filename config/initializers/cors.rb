8f6f5e02312e6c5f462ac04b5df29f67cee4b39b
BMoClay <benmorgancleveland@gmail.com>
2021-08-24 17:23:12 -0400

    cors adjustment change origins back to netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'https://raregifs.netlify.app'
+    # origins '*'
 
     resource '*',
       headers: :any,
