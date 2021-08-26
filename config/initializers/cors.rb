d6e6dc91a08b7fde2ecaffab1c9867da657deb88
BMoClay <benmorgancleveland@gmail.com>
2021-08-26 10:27:23 -0400

    change origins back to raregifs.netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://raregifs.netlify.app'
+     origins 'https://raregifs.netlify.app'
     # origins '*'
 
     resource '*',
