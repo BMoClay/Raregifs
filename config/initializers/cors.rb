6ff4c85c85c4b79f858cee95d6daf5784532198b
BMoClay <benmorgancleveland@gmail.com>
2021-09-17 16:20:59 -0400

    rails transition to firebase start

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-      origins 'https://raregifs.netlify.app'
-      # origins '*'
+      #origins 'https://raregifs.netlify.app'
+       origins '*'
 
     resource '*',
       headers: :any,
